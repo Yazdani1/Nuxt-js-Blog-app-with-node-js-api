6224bc69ef64acafa1bbd2a0ce2c154fbabcc852
Yazdani <yaz4dani@gmail.com>
2021-09-20 12:45:40 +0200

    nodeInstalled

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Nuxt blog app")
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+require('./models/db');
+
+
+
+app.listen(PORT,(req,res)=>{
+    console.log("Server Connected")
+})
+
