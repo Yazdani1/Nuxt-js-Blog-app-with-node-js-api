1e3bca476b1e130b079dd4f9cc26c261839e15f5
Yazdani <yaz4dani@gmail.com>
2021-09-21 15:10:01 +0200

    bootstrapNavigation

---
+++ client/layouts/nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" style="color:white" href="#">Blog App</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ">
+            <a class="nav-link active " style="color:white"  aria-current="page" href="#">Post Data</a>
+            <a class="nav-link" style="color:white" href="#">News Feed</a>
+            <a class="nav-link" style="color:white" href="#">All Post</a>
+            
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
